a41c9fe93964bc35d698df1be7ecd1c966710819
Joselyn2114 <pjoselyn2114@gmail.com>
2025-04-16 19:33:12 -0600

    Implement http request reading and parsing

---
+++ http_request.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/url"
+	"strings"
+)
+
+type HttpRequest struct {
+	Method string
+	Target *url.URL
+	Header map[string]string
+	Body   string
+}
+
+func NewHttpRequest(method string, target *url.URL, header map[string]string, body string) *HttpRequest {
+	return &HttpRequest{
+		Method: method,
+		Target: target,
+		Header: header,
+		Body:   body,
+	}
+}
+
+func ReadRequest(conn net.Conn) (*HttpRequest, error) {
+	lines := make([]string, 0)
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return nil, err
+		}
+
+		line = strings.TrimSuffix(line, "\r\n")
+		line = strings.TrimSuffix(line, "\n")
+
+		if line == "" {
+			break
+		}
+
+		lines = append(lines, line)
+	}
+
+	if len(lines) == 0 {
+		return nil, fmt.Errorf("")
+	}
+
+	headersPart := strings.Join(lines, "\n") + "\n\n"
+
+	request, err := ParseRequest(headersPart)
+	if err != nil {
+		return nil, err
+	}
+
+	if contentLengthStr, ok := request.Header["Content-Length"]; ok {
+		var contentLength int
+
+		_, err := fmt.Sscan(contentLengthStr, &contentLength)
+		if err != nil {
+			return nil, err
+		}
+
+		if contentLength > 0 {
+			body := make([]byte, contentLength)
+
+			_, err := io.ReadAtLeast(reader, body, contentLength)
+			if err != nil {
+				return nil, err
+			}
+
+			request.Body = string(body)
+		}
+	}
+
+	return request, nil
+}
+
+func ParseRequest(headersPart string) (*HttpRequest, error) {
+	headerEndIndex := strings.Index(headersPart, "\n\n")
+	if headerEndIndex == -1 {
+		return nil, fmt.Errorf("")
+	}
+
+	headerPart := headersPart[:headerEndIndex]
+
+	lines := strings.Split(headerPart, "\n")
+	if len(lines) < 1 {
+		return nil, fmt.Errorf("")
+	}
+
+	start := strings.SplitN(lines[0], " ", 3)
+	if len(start) < 2 {
+		return nil, fmt.Errorf("")
+	}
+
+	method := start[0]
+	if method == "" {
+		return nil, fmt.Errorf("")
+	}
+
+	targetStr := start[1]
+	if targetStr == "" {
+		return nil, fmt.Errorf("")
+	}
+
+	target, err := url.Parse(targetStr)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := make(map[string]string)
+
+	for _, line := range lines[1:] {
+		parts := strings.SplitN(line, ":", 2)
+
+		if len(parts) != 2 {
+			continue
+		}
+
+		k := strings.TrimSpace(parts[0])
+		v := strings.TrimSpace(parts[1])
+		if k == "" {
+			continue
+		}
+
+		headers[k] = v
+	}
+
+	body := ""
+
+	return NewHttpRequest(method, target, headers, body), nil
+}
